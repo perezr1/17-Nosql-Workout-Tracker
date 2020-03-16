b9d99f3fc161e92f7a2fb6ebf495c3dd078f2f8c
“Ronny <contact@ronnyperez.com>
2020-03-15 22:37:39 -0500

    fixed server.js to ensure local host is listening. Started API working.

---
+++ routes/api.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.post("/api/workouts", (req, res) => {
+  Workout.create({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
-
 const User = require("./models");
 const app = express();
 
@@ -11,14 +10,21 @@ app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
+
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", {
-  useNewUrlParser: true
-});
-
-require("./routes/api")(app);
-require("./routes/view")(app);
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", {
+    // app requested update
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .catch(err => {
+    throw err;
+  });
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
